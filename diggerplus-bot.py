ecda707fa9c2fe1f44f67e7f9464be8105f81721
kiven <kiven.mr@gmail.com>
2016-10-09 12:31:31 +0800

    R (#4)
    Refactor

--- diggerplus-bot.py
+++ diggerplus-bot.py
@@ -1,82 +1,74 @@
 # -*- coding: utf-8 -*-
 
-import time
 import re
+import random
+import redis
 
-from slackclient import SlackClient
+from slackbot.bot import respond_to
+from slackbot.bot import listen_to
+from google import lucky
 
-# config
-token = "slack bot token"
-bot_name = 'diggerplus-bot'
-bot_id = 'slack bot id'
+r = redis.Redis()
 
-# consts
-READ_WEBSOCKET_DELAY = 1
+HI_MSGS = [
+    'Hello gay!',
+    'Are you kidding me?'
+]
 
-HELP_COMMADN = '!help'
-SET_COMMAND = '!set'
-EXAMPLE_COMMAND = 'do'
-PATTERN = r'!set (\w+) (.*)'
-
-slack_client = SlackClient(token)
-
-##
-# Memory dict to store keywords
-##
-MEMORY_DICT = {}
-
-# This is just to get bot id
-"""
-api_call = slack_client.api_call("users.list")
-if api_call.get('ok'):
-    users = api_call.get('members')
-    for user in users:
-        if 'name' in user and user.get('name') == bot_name:
-            print 'bot_id: ', user.get('id')
-"""
+KEYWORD_PREFIX = 'slackbot:bot:keyword:%s'
+ALL_KEYWORDS = 'slackbot:bot:keywords'
 
 
-def handle_command(command, channel):
-    """Handler for parser commands."""
-
-    resp = "Not sure what you mean. Try to type `!help` to get help!"
-    print 'Current channel: {!r}, command: {!r}'.format(channel, command)
-    if command == HELP_COMMADN:
-        resp = '`{}`'.format(PATTERN)
-    elif command.startswith(SET_COMMAND):
-        matchs = re.compile(PATTERN).match(command).groups()
-        key, val = matchs
-        # if key in MEMORY_DICT:
-        #     resp = "Keyword: {!r} is already exists. But I will override it!"
-        MEMORY_DICT[key] = val
-        resp = "Got it!"
-    else:
-        key = command.split('!')[1]
-        _resp = MEMORY_DICT.get(key)
-        if _resp is not None:
-            resp = _resp
-    slack_client.api_call("chat.postMessage", channel=channel, text=resp,
-                          as_user=True)
+@respond_to('^hi$', re.IGNORECASE)
+def hi(message):
+    message.reply(random.choice(HI_MSGS))
+    message.react('+1')
 
 
-def parse_slack_uptput(slack_rtm_output):
-    """Filter commands by symbol `!`."""
-
-    output_list = slack_rtm_output
-    if output_list and len(output_list) > 0:
-        for output in output_list:
-            if output and 'text' in output and output['text'].startswith('!'):
-                return output['text'].lower(), output['channel']
-    return None, None
+@respond_to('I loven you')
+def love(message):
+    message.reply('I love you too!')
 
 
-if __name__ == '__main__':
-    if slack_client.rtm_connect():
-        print "StarterBot conected and running!"
-        while True:
-            command, channel = parse_slack_uptput(slack_client.rtm_read())
-            if command and channel:
-                handle_command(command, channel)
-            time.sleep(READ_WEBSOCKET_DELAY)
-    else:
-        print "Connection Failed, invalid token?"
+@listen_to('^help$')
+def help(message):
+    message.reply("Waht's your problem?")
+
+
+@respond_to('^!(\w+)$')
+@listen_to('^!(\w+)$')
+def keyword_lookup(message, keyword):
+    resp = r.get(KEYWORD_PREFIX % keyword)
+    if not resp:
+        return message.send("Not sure what you mean.")
+    message.send(resp)
+
+
+@respond_to('^!set (\w+) (.+)$')
+@listen_to('^!set (\w+) (.+)$')
+def set_keyword(message, keyword, value):
+    r.set(KEYWORD_PREFIX % keyword, value)
+    message.send('Got it!')
+    r.sadd(ALL_KEYWORDS, keyword)
+
+
+@listen_to("^!list keywords$")
+@respond_to("^!list keywords$")
+def all_keywords(message):
+    message.send(','.join(r.smembers(ALL_KEYWORDS)))
+
+
+@listen_to("^!google (.*)$")
+@respond_to("^!google (.*)$")
+def google(message, keyword):
+    message.send("http://lmgtfy.com/?q={}".format("+".join(keyword.split())))
+
+
+@listen_to("^!g (.*)$")
+@respond_to("^!g (.*)$")
+def google_lucky(message, keyword):
+    r = lucky(keyword)
+    if r:
+        url, desc = r
+        return message.send(u"{} - {}".format(url, desc))
+    return message.send('Nothing found!')
---
+++ slackbot_settings.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+API_TOKEN = 'your slack token'
+
+DEFAULT_REPLY = "Not sure what you mean."
+
+PLUGINS = [
+    'slackbot.plugins',
+    'diggerplus-bot',
+]
